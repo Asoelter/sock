0046b0271be8cca3c3d8c9af0f612db981c5579a
asoelter <adamdlsoelter@gmail.com>
2022-12-24 14:33:18 -0600

    Cleanup: Move MessageBase into an impl file

--- nylon/nylon_message_base.h
+++ nylon/nylon_message_base.h
@@ -19,75 +19,22 @@ struct MessageBase : Fs...
 
     static constexpr auto messageType = MessageType;
 
-    size_t size() const noexcept
-    {
-        return sizeof(MessageTypeT) + (static_cast<Fs const&>(*this).size() + ... + 0);
-    }
-
-    size_t encodeSize() const noexcept
-    {
-        return sizeof(MessageTypeT) + (static_cast<Fs const&>(*this).encodeSize() + ... + 0);
-    }
+    size_t size() const noexcept;
+    size_t encodeSize() const noexcept;
 
     template <typename MemberField>
-    auto& field()
-    {
-        static_assert(
-            std::is_base_of_v<Field<typename MemberField::ValueType, typename MemberField::Archetype>, MemberField>,
-            "Asked for a type that is not a field"
-        );
-
-        static_assert(
-            Contains<Fields, MemberField>,
-            "Asked for a field that does not belong to this message"
-        );
-
-        return static_cast<MemberField&>(*this);
-    }
+    MemberField& field();
 
     template <typename MemberField>
-    auto const & field() const
-    {
-        static_assert(
-            std::is_base_of_v<Field<typename MemberField::ValueType, typename MemberField::Archetype>, MemberField>,
-            "Asked for a type that is not a field"
-        );
-
-        static_assert(
-            Contains<Fields, MemberField>,
-            "Asked for a field that does not belong to this message"
-        );
-
-        return static_cast<MemberField&>(*this);
-    }
+    MemberField const & field() const;
 
     // TODO(asoelter): overload for non-const
     template <typename Visitor>
-    void forEachField(Visitor const & visitor) const
-    {
-        // There really never should be any empty messages, but
-        // this could save some annoying debuggin in the future
-        if constexpr (!std::is_same_v<TypeList<Fs...>, EmptyList>) {
-            forEachFieldImpl<Visitor, TypeList<Fs...>>(visitor);
-        }
-    }
+    void forEachField(Visitor const & visitor) const;
 
 private:
     template <typename Visitor, typename ListOfFields>
-    void forEachFieldImpl(Visitor const& visitor) const
-    {
-        using CurrentField = Head<ListOfFields>;
-
-        auto const & currentField = static_cast<CurrentField const&>(*this);
-
-        visitor(currentField);
-
-        using RestOfFields = Tail<ListOfFields>;
-
-        if constexpr (!std::is_same_v<RestOfFields, EmptyList>) {
-            forEachFieldImpl<Visitor, RestOfFields>(visitor);
-        }
-    }
+    void forEachFieldImpl(Visitor const& visitor) const;
 };
 
 
--- nylon/nylon_message_base_inline.h
+++ nylon/nylon_message_base_inline.h
@@ -1,3 +1,77 @@
 NYLON_NAMESPACE_BEGIN
 
+template <MessageTypeT MessageType, typename ... Fs>
+size_t MessageBase<MessageType, Fs...>::size() const noexcept
+{
+    return sizeof(MessageTypeT) + (static_cast<Fs const&>(*this).size() + ... + 0);
+}
+
+template <MessageTypeT MessageType, typename ... Fs>
+size_t MessageBase<MessageType, Fs...>::encodeSize() const noexcept
+{
+    return sizeof(MessageTypeT) + (static_cast<Fs const&>(*this).encodeSize() + ... + 0);
+}
+
+template <MessageTypeT MessageType, typename ... Fs>
+template <typename MemberField>
+MemberField& MessageBase<MessageType, Fs...>::field()
+{
+    static_assert(
+        std::is_base_of_v<Field<typename MemberField::ValueType, typename MemberField::Archetype>, MemberField>,
+        "Asked for a type that is not a field"
+    );
+
+    static_assert(
+        Contains<Fields, MemberField>,
+        "Asked for a field that does not belong to this message"
+    );
+
+    return static_cast<MemberField&>(*this);
+}
+
+template <MessageTypeT MessageType, typename ... Fs>
+template <typename MemberField>
+MemberField const & MessageBase<MessageType, Fs...>::field() const
+{
+    static_assert(
+        std::is_base_of_v<Field<typename MemberField::ValueType, typename MemberField::Archetype>, MemberField>,
+        "Asked for a type that is not a field"
+    );
+
+    static_assert(
+        Contains<Fields, MemberField>,
+        "Asked for a field that does not belong to this message"
+    );
+
+    return static_cast<MemberField&>(*this);
+}
+
+template <MessageTypeT MessageType, typename ... Fs>
+template <typename Visitor>
+void MessageBase<MessageType, Fs...>::forEachField(Visitor const & visitor) const
+{
+    // There really never should be any empty messages, but
+    // this could save some annoying debuggin in the future
+    if constexpr (!std::is_same_v<TypeList<Fs...>, EmptyList>) {
+        forEachFieldImpl<Visitor, TypeList<Fs...>>(visitor);
+    }
+}
+
+template <MessageTypeT MessageType, typename ... Fs>
+template <typename Visitor, typename ListOfFields>
+void MessageBase<MessageType, Fs...>::forEachFieldImpl(Visitor const& visitor) const
+{
+    using CurrentField = Head<ListOfFields>;
+
+    auto const & currentField = static_cast<CurrentField const&>(*this);
+
+    visitor(currentField);
+
+    using RestOfFields = Tail<ListOfFields>;
+
+    if constexpr (!std::is_same_v<RestOfFields, EmptyList>) {
+        forEachFieldImpl<Visitor, RestOfFields>(visitor);
+    }
+}
+
 NYLON_NAMESPACE_END
